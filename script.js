3ee6d32d1d1ce96b9c2ee6be0845c1ba04f52093
Chiazamoku-Amadi <amadichiaza98@gmail.com>
2022-06-13 20:53:54 +0100

    Added all files

---
+++ script.js
@@ -0,0 +1,70 @@
+var keyA = document.getElementById('a')
+var audio1 = document.getElementById('audio1')
+
+keyA.addEventListener('click', function() {
+    audio1.play();
+})
+
+
+var keyS = document.getElementById('s')
+var audio2 = document.getElementById('audio2')
+
+keyS.addEventListener('click', function() {
+    audio2.play();
+})
+
+
+var keyD = document.getElementById('d')
+var audio3 = document.getElementById('audio3')
+
+keyD.addEventListener('click', function() {
+    audio3.play();
+})
+
+
+var keyF = document.getElementById('f')
+var audio4 = document.getElementById('audio4')
+
+keyF.addEventListener('click', function() {
+    audio4.play();
+})
+
+
+var keyG = document.getElementById('g')
+var audio5 = document.getElementById('audio5')
+
+keyG.addEventListener('click', function() {
+    audio5.play();
+})
+
+
+var keyH = document.getElementById('h')
+var audio6 = document.getElementById('audio6')
+
+keyH.addEventListener('click', function() {
+    audio6.play();
+})
+
+
+var keyJ = document.getElementById('j')
+var audio7 = document.getElementById('audio7')
+
+keyJ.addEventListener('click', function() {
+    audio7.play();
+})
+
+
+var keyK = document.getElementById('k')
+var audio8 = document.getElementById('audio8')
+
+keyK.addEventListener('click', function() {
+    audio8.play();
+})
+
+
+var keyL = document.getElementById('l')
+var audio9 = document.getElementById('audio9')
+
+keyL.addEventListener('click', function() {
+    audio9.play();
+})
